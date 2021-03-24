ebf390b6a2a42d6c599f1a119cbcd629a2ce0969
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-03-24 23:58:11 +0530

    Basic Commit

---
+++ src/components/body/imagecard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './imagecard.css';
+
+const imageCard = ()=>{
+    
+    
+    return(
+        <div className = 'container'>
+            <div className = "image">
+                <pre>
+                </pre>
+            </div>
+        </div>
+    )
+}
+
+export default imageCard
